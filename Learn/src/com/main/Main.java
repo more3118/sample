8065541ed38f3e5e8d06907ff292b5ffe6f6f02e
Shreyas More <more3118@gmail.com>
2024-11-19 16:50:45 +0000

    Adding Source Code

---
+++ Learn/src/com/main/Main.java
@@ -0,0 +1,16 @@
+package com.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int dividend = 3;
+		int divisor = 2;
+
+		double result = dividend / divisor * 1.0;
+		System.out.println(result);
+
+	}
+
+}
